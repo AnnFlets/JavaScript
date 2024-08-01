29fe9749b52777928b8c1f7bb7c8e02a10b59122
AnnFlets <analuciaflets@gmail.com>
2024-07-31 21:52:37 -0600

    31-07-2024 --- Ejercicios y preguntas

--- 8/ejercicio.js
+++ 8/ejercicio.js
@@ -1,6 +1,9 @@
 let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
 numbers.push(6);
+console.log(numbers)
 let num = numbers.shift();
+console.log(numbers);
 numbers.forEach((numero) => {
     console.log(numero);
 });
