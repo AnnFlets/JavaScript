ea09b4bba6c7d2fb392044032a08139a3252e9c9
AnnFlets <analuciaflets@gmail.com>
2024-08-02 21:41:54 -0600

    02-08-2024 --- Ejercicio tablero con movimiento personaje

--- 9/script.js
+++ 9/script.js
@@ -30,26 +30,38 @@ function imprimirMatriz(dimension1, dimension2, matriz){
     console.log(strMatriz);
 }
 
-
-
 //EJERCICIO #2
+let tablero = [];
+let cantTrampas = [];
+let trampasTablero = [];
+const dimension1 = 8;
+const dimension2 = 8;
+let numeroDado = 0;
+let casillaPersonaje = 1;
+
 function segundoEjercicio() {
-    let dimension1 = 8;
-    let dimension2 = 8;
-    let tablero = generarTablero(dimension1, dimension2);
-    let cantTrampas = generarCantidadTrampasSeccion(dimension1);
-    let trampasTablero = generarCasillasConTrampas(dimension1, cantTrampas);
-    imprimirTablero(dimension1, dimension2, tablero, trampasTablero);
+    limpiarVariables();
+    generarTablero();
+    generarCantidadTrampasSeccion();
+    generarCasillasConTrampas();
+    imprimirTablero();
     console.log("Trampas por sección: " + cantTrampas);
     console.log("Casillas con trampa: " + trampasTablero);
+}
+
+function limpiarVariables(){
+    tablero = [];
+    cantTrampas = [];
+    trampasTablero = [];
+    numeroDado = 0;
+    casillaPersonaje = 1;
 }
 
 /*
 Función que se encarga de generar la matriz que contendrá 
 el tablero de juego de 8x8.
 */
-function generarTablero(dimension1, dimension2){
-    let matriz = [];
+function generarTablero(){
     for (let i = 0; i < dimension1; i++) {
         let arreglo = [];
         for (let j = 0; j < dimension2; j++) {
@@ -59,9 +71,8 @@ function generarTablero(dimension1, dimension2){
                 arreglo.unshift((j + 1) + (i * dimension2));
             }
         }
-        matriz.push(arreglo);
+        tablero.push(arreglo);
     }
-    return matriz;
 }
 
 /*
@@ -71,14 +82,12 @@ un mínimo de 2 y un máximo de 5 trampas por sección.
 La cantidad de trampas por sección se almacena en un
 arreglo.
 */
-function generarCantidadTrampasSeccion(dimension1){
-    let trampas = [];
+function generarCantidadTrampasSeccion(){
     const min = 2;
     const max = 5;
     for(let i = 0; i < (dimension1/2); i++){
-        trampas.push(Math.floor(Math.random() * (max - min + 1) + min));
+        cantTrampas.push(Math.floor(Math.random() * (max - min + 1) + min));
     }
-    return trampas;
 }
 
 /*
@@ -87,53 +96,87 @@ de las casillas a las cuales se les ha asignado una trampa,
 tomando en cuenta que dichos valores no se repitan y correspondan
 a la sección del tablero correspondiente.
 */
-function generarCasillasConTrampas(dimension1, cantidadTrampas){
+function generarCasillasConTrampas(){
     let min = 1;
     let max = (dimension1 * 2);
-    let casillasTrampa = [];
     let casilla = 0;
-    cantidadTrampas.forEach((trampas) => {
+    cantTrampas.forEach((trampas) => {
         let noRepetidas = 0;
         while(noRepetidas !== trampas){
             casilla = Math.floor(Math.random() * (max - min + 1) + min);
-            if(!(casillasTrampa.includes(casilla))){
-                casillasTrampa.push(casilla)
+            if(!(trampasTablero.includes(casilla))){
+                trampasTablero.push(casilla)
                 noRepetidas += 1;
             }
         }
-        min = min + (dimension1 * 2);
-        max = max + (dimension1 * 2);
+        min += (dimension1 * 2);
+        max += (dimension1 * 2);
     });
-    return casillasTrampa;
 }
 
 /*
 Función que se encarga de imprimir el tablero, mostrando las casillas,
-el número que identifica a las mismas, y las trampas existentes en
-algunas de estas.
+el número que identifica a las mismas, las trampas existentes en
+algunas de estas y la casilla en la que se encuentra el jugador.
 */
-function imprimirTablero(dimension1, dimension2, tablero, trampas){
+function imprimirTablero(){
+    casillaPersonaje += numeroDado;
+    if(casillaPersonaje > dimension1*dimension2){
+        console.log("¡FELICIDADES! Terminó el juego");
+        limpiarVariables();
+        return
+    }
     let strTablero = "";
     const strDivision = "-----------------------------------------------------------------";
     let strFila = "";
     let strFilaTrampa = "";
-    for(let i = (dimension1 - 1); i > -1; i--){
+    let strFilaPersonaje = "";
+    let esTrampa = false;
+    for(let i = (tablero.length - 1); i > -1; i--){
         strFila = "";
         strFilaTrampa = "";
-        for(let j = 0; j < dimension2; j++){
+        strFilaPersonaje = "";
+        for(let j = 0; j < tablero[i].length; j++){
             strFila += "|\t" + tablero[i][j] + "\t";
-            if(trampas.includes(tablero[i][j])){
+            if(trampasTablero.includes(tablero[i][j])){
                 strFilaTrampa += "|\t#\t";
             }else{
                 strFilaTrampa += "|\t \t";
             }
-            if(j === (dimension2 - 1)){
+            if(casillaPersonaje === tablero[i][j]){
+                strFilaPersonaje += "|\t@\t";
+            }else{
+                strFilaPersonaje += "|\t \t";
+            }
+            if(j === (tablero[i].length - 1)){
                 strFila += "|";
                 strFilaTrampa += "|";
+                strFilaPersonaje += "|";
+            }
+            if(trampasTablero.includes(casillaPersonaje)){
+                esTrampa = true;
             }
         }
-        strTablero += strDivision + "\n" + strFila + "\n" + strFilaTrampa + "\n";
+        strTablero += strDivision + "\n" + strFila + "\n" + strFilaTrampa + "\n" + strFilaPersonaje + "\n";
     }
     strTablero += strDivision;
     console.log(strTablero);
+    if(esTrampa){
+        console.log("¡UPS! Cayó en una trampa")
+    }
+}
+
+/*
+Función que se encarga de generar un número aleatorio entre 1 y 6,
+tomando en cuenta que este solo se generará si se ha creado un
+tablero de juego con anterioridad.
+*/
+function lanzarDado(){
+    if(tablero.length < 1){
+            console.log("¡ERROR! Debe generar un nuevo tablero")
+    }else{
+        numeroDado = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        console.log("DADO LANZADO - Número obtenido: " + numeroDado);
+        imprimirTablero();
+    }
 }
